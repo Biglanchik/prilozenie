c9609f358c45058d30f201c58d55757aaf3b6ff9
Имя Фамилия <vanek.semenov.90@mail.ru>
2021-12-09 01:07:52 +0300

    Добавьте файлы проекта.

---
+++ zametki/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace zametki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dobavit = new System.Windows.Forms.Button();
+            this.zagr = new System.Windows.Forms.Button();
+            this.sohr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dobavit
+            // 
+            this.dobavit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dobavit.Location = new System.Drawing.Point(713, 24);
+            this.dobavit.Name = "dobavit";
+            this.dobavit.Size = new System.Drawing.Size(75, 23);
+            this.dobavit.TabIndex = 0;
+            this.dobavit.Text = "Добавить";
+            this.dobavit.UseVisualStyleBackColor = true;
+            this.dobavit.Click += new System.EventHandler(this.dobavit_Click);
+            // 
+            // zagr
+            // 
+            this.zagr.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.zagr.Location = new System.Drawing.Point(713, 105);
+            this.zagr.Name = "zagr";
+            this.zagr.Size = new System.Drawing.Size(75, 23);
+            this.zagr.TabIndex = 1;
+            this.zagr.Text = "Загрузить";
+            this.zagr.UseVisualStyleBackColor = true;
+            // 
+            // sohr
+            // 
+            this.sohr.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sohr.Location = new System.Drawing.Point(713, 64);
+            this.sohr.Name = "sohr";
+            this.sohr.Size = new System.Drawing.Size(75, 23);
+            this.sohr.TabIndex = 2;
+            this.sohr.Text = "Сохранить";
+            this.sohr.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sohr);
+            this.Controls.Add(this.zagr);
+            this.Controls.Add(this.dobavit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dobavit;
+        private System.Windows.Forms.Button zagr;
+        private System.Windows.Forms.Button sohr;
+    }
+}
+
---
+++ zametki/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zametki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dobavit_Click(object sender, EventArgs e)
+        {
+            TextBox dobav = new TextBox();
+            this.Controls.Add(dobav);
+            dobav.Location = new Point(100, 200);
+            dobav.MouseMove += TextBox1_MouseMove;
+            dobav.MouseHover += TextBox1_MouseHover;
+            
+
+
+        }
+
+        private void TextBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            TextBox textBox1 = new TextBox();
+            Control dobav = sender as Control;
+
+
+
+
+            dobav.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            if (e.Button == MouseButtons.Left)
+            {
+                dobav.Location = this.PointToClient(Control.MousePosition);
+            }
+
+        }
+
+        private void TextBox1_MouseHover(object sender, EventArgs e)
+        {
+            Control dobav = sender as Control;
+            ToolTip t = new ToolTip();
+            t.SetToolTip(dobav, "Подсказка для TextBox");
+            
+        }
+
+    }
+}
